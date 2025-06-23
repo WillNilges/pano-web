2555f188ce5b0abff0a9c6684849826f5148d8fd
Will Nilges <willardnilges@gmail.com>
2025-06-23 08:11:36 -0400

    additional images poc

--- src/components/PanoramaCardList/PanoramaCardList.tsx
+++ src/components/PanoramaCardList/PanoramaCardList.tsx
@@ -4,31 +4,50 @@ import styles from "./PanoramaCardList.module.scss";
 import { getPanoEndpoint } from "@/lib/server";
 
 interface PanoramaViewerProps {
-  images: any;
+    images: any;
+    additional_images: any;
 }
 
 export default async function PanoramaCardList({
-  images,
+    images,
+    additional_images,
 }: PanoramaViewerProps) {
-  const panoEndpoint = (await getPanoEndpoint()) || "";
-  return (
-    <>
-      <div className={styles.panoramaList}>
-        {images.length > 0 &&
-          images.map((image: Image, index: any) => (
-            <div key={image.id}>
-              <PanoramaViewerCard
-                key={image.id}
-                id={image.id}
-                originalFilename={image.original_filename}
-                timestamp={image.timestamp}
-                category={image.category}
-                url={image.url}
-                panoEndpoint={panoEndpoint}
-              />
+    const panoEndpoint = (await getPanoEndpoint()) || "";
+    return (
+        <>
+            <div className={styles.panoramaList}>
+                {images.length > 0 &&
+                    images.map((image: Image, index: any) => (
+                        <div key={image.id}>
+                            <PanoramaViewerCard
+                                key={image.id}
+                                id={image.id}
+                                originalFilename={image.original_filename}
+                                timestamp={image.timestamp}
+                                category={image.category}
+                                url={image.url}
+                                panoEndpoint={panoEndpoint}
+                            />
+                        </div>
+                    ))}
             </div>
-          ))}
-      </div>
-    </>
-  );
+            <h3> Additional Images </h3>
+            <div className={styles.panoramaList}>
+                {additional_images.length > 0 &&
+                    additional_images.map((image: Image, index: any) => (
+                        <div key={image.id}>
+                            <PanoramaViewerCard
+                                key={image.id}
+                                id={image.id}
+                                originalFilename={image.original_filename}
+                                timestamp={image.timestamp}
+                                category={image.category}
+                                url={image.url}
+                                panoEndpoint={panoEndpoint}
+                            />
+                        </div>
+                    ))}
+            </div>
+        </>
+    );
 }
--- src/components/PanoramaViewer/PanoramaViewer.tsx
+++ src/components/PanoramaViewer/PanoramaViewer.tsx
@@ -1,8 +1,8 @@
 "use server";
 import {
-  ModelType,
-  modelTypeToAPIRouteMap,
-  modelTypeToLabelMap,
+    ModelType,
+    modelTypeToAPIRouteMap,
+    modelTypeToLabelMap,
 } from "@/lib/types";
 import { getPanoEndpoint } from "@/lib/server";
 import PanoramaCardList from "@/components/PanoramaCardList/PanoramaCardList";
@@ -12,44 +12,44 @@ import styles from "./PanoramaViewer.module.scss";
 
 // Retrieves images from the Pano API
 export async function getImages(modelNumber: number, modelType: ModelType) {
-  if (modelNumber === undefined) {
-    console.warn("modelNumber undefined. Aborting Pano API query");
-    return [];
-  }
+    if (modelNumber === undefined) {
+        console.warn("modelNumber undefined. Aborting Pano API query");
+        return [];
+    }
 
-  console.log(
-    `Querying for modelType = ${modelType}, modelNumber = ${modelNumber}`,
-  );
-  const fetchURL = `${await getPanoEndpoint()}/api/v1/${modelTypeToAPIRouteMap.get(modelType)}/${modelNumber}`;
-  const response = await fetch(fetchURL, {
-    credentials: "include",
-  });
-
-  if (!response.ok) {
-    throw new Error(
-      `Could not fetch images. HTTP Request to Pano failed. Status: ${response.status}`,
+    console.log(
+        `Querying for modelType = ${modelType}, modelNumber = ${modelNumber}`,
     );
-  }
-  const images = await response.json();
-  console.log(`Retrieved images from ${fetchURL}`);
-  return images;
+    const fetchURL = `${await getPanoEndpoint()}/api/v1/${modelTypeToAPIRouteMap.get(modelType)}/${modelNumber}`;
+    const response = await fetch(fetchURL, {
+        credentials: "include",
+    });
+
+    if (!response.ok) {
+        throw new Error(
+            `Could not fetch images. HTTP Request to Pano failed. Status: ${response.status}`,
+        );
+    }
+    const image_response_json = await response.json();
+    console.log(`Retrieved images from ${fetchURL}`);
+    return image_response_json;
 }
 
 interface PanoramaViewerProps {
-  modelNumber: number;
-  modelType: ModelType;
+    modelNumber: number;
+    modelType: ModelType;
 }
 
 export default async function PanoramaViewer({
-  modelNumber,
-  modelType,
+    modelNumber,
+    modelType,
 }: PanoramaViewerProps) {
-  const images = await getImages(modelNumber, modelType);
-  return (
-    <>
-      <PanoHeader />
-      <SearchBar modelNumber={modelNumber} modelType={modelType} />
-      <PanoramaCardList images={images} />
-    </>
-  );
+    const { images, additional_images } = await getImages(modelNumber, modelType);
+    return (
+        <>
+            <PanoHeader />
+            <SearchBar modelNumber={modelNumber} modelType={modelType} />
+            <PanoramaCardList images={images} additional_images={additional_images} />
+        </>
+    );
 }
