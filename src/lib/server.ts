7773a99ad1491fc2c366c590eb2a3f74771fd45f
Willard Nilges <willardnilges@gmail.com>
2025-08-28 08:50:18 -0400

    update action

--- src/lib/server.ts
+++ src/lib/server.ts
@@ -3,7 +3,7 @@
 export async function getPanoEndpoint() {
   const endpoint = process.env.PANO_ENDPOINT;
   if (endpoint === undefined || endpoint === '') {
-    throw new Error('PANO_ENDPOINT is undefined.');
+    throw new Error('PANO_ENDPOINT is undefined. Check your env vars.');
   }
   return endpoint;
 }
